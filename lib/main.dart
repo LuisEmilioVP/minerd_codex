20f9667c9d318cf809ee9bea6480d02807bfac94
Fraimer <107742206+FraimerA@users.noreply.github.com>
2024-08-06 15:26:26 -0400

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,19 @@
-import 'views/home.dart';
-import 'views/login.dart';
-import 'views/register.dart';
-import 'views/pages/perfil.dart';
-import 'views/visitas/visitas_page.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'views/pages/change_password.dart';
-import 'views/pages/recover_password.dart';
-import 'controllers/usuario_controller.dart';
-import 'controllers/visita_controller.dart';
+import 'controllers/video_controller.dart';
+import 'views/video_listScreen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (context) => UsuarioController()),
-        ChangeNotifierProvider(create: (context) => VisitaController()),
-      ],
+    return ChangeNotifierProvider(
+      create: (_) => VideoController(),
       child: MaterialApp(
-        title: 'Mi Aplicación',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-          visualDensity: VisualDensity.adaptivePlatformDensity,
-        ),
-        home: const MyLogin(),
-        routes: {
-          '/home': (context) => const Home(),
-          '/login': (context) => const MyLogin(),
-          '/register': (context) => const MyRegister(),
-          '/recover_password': (context) => const RecoverPasswordPage(),
-          '/change_password': (context) => const ChangePasswordPage(),
-          '/perfil': (context) => const Perfil(),
-          '/mis_visitas': (context) => const MisVisitasPage(),
-        },
+        home: VideoListScreen(),
       ),
     );
   }
